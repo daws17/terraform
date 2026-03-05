08ccf5080d8935d32efa64eecbfb4cfb81db357e
sai kiran <saikiranakula17@gmail.com>
2026-03-06 04:14:11 +0530

    terraform

---
+++ count/variable.tf
@@ -0,0 +1,14 @@
+variable "instances" {
+    default = [ "mongodb" , "redis" , "rabbitmq" ]
+}
+
+variable "zone_id" {
+    default = "Z001445138PJ8LEVGMQY"
+}
+
+
+variable "domain_name" {
+    default = "devops-practice.space"
+}
+
+
